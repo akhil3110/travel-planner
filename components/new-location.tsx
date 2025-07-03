4f8b52a72464bea73c170f2e400f17597e4b49ae
Akhil <akhil1659@gmail.com>
2025-07-03 23:18:37 +0530

    added add location page

--- components/new-location.tsx
+++ components/new-location.tsx
@@ -2,8 +2,12 @@
 
 const NewLocationClient = ({tripId}: {tripId: string}) => {
     return ( 
-        <div>
+        <div className="min-h-[calc(100vh-8rem)] flex items-center justify-center bg-gray-50">
+            <div className="mx-auto w-full max-w-md">
+                <div className="bg-white">
 
+                </div>
+            </div>
         </div>
      );
 }
