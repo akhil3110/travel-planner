80d763880f8dd38c4998448424708264b0b7f6a9
Akhil <akhil1659@gmail.com>
2025-07-13 00:19:18 +0530

    globe page completed

---
+++ app/api/trips/route.ts
@@ -0,0 +1,48 @@
+import { auth } from "@/auth";
+import { getCountryFromCordinates } from "@/lib/actions/geoCode";
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET () {
+    try {
+        
+        const session = await auth()
+        if(!session){
+            return new NextResponse("Not Authenticated", {status:401})
+        }
+
+        const locations = await prisma.location.findMany({
+            where: {
+                trip : {
+                    userId: session.user?.id
+                }
+            },
+            select: {
+                locationTitle: true,
+                lat: true,
+                lng: true,
+                trip: {
+                    select: {
+                        title: true
+                    }
+                }
+            }
+        });
+
+        const tranformedLocation = await Promise.all(locations.map(async(loc) =>{
+            const geoCodeResult = await getCountryFromCordinates(loc.lat,loc.lng)
+            
+            return {
+                name: `${loc.trip.title} - ${geoCodeResult.formattedAddress}`,
+                lat: loc.lat,
+                lng: loc.lng,
+                country: geoCodeResult.country
+            }
+        }))
+
+        return NextResponse.json(tranformedLocation)
+
+    } catch (error) {
+        return new NextResponse("Internal Error", {status:500})
+    }
+}
